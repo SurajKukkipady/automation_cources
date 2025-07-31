e79f107bb151d3e0aa196050bec786c47b754d69
SurajKukkipady <56889466+SurajKukkipady@users.noreply.github.com>
2025-07-31 11:30:03 +0530

    class based test

--- pytest_practice/tests/test_ciricle.py
+++ pytest_practice/tests/test_ciricle.py
@@ -1,8 +1,18 @@
 import src.shapes as shapes
 import pytest
+import math
 
 class TestCircle:
-    
 
-    def test_one(self):
-        assert True
+    def setup_method(self, method):
+        print(f"Setting up for {method}")
+        self.circle = shapes.Circle(5)
+
+    def teardown_method(self, method):
+        print(f"Tearing down after {method}")
+
+    def test_radius(self):
+        assert self.circle.area() == math.pi * self.circle.radius ** 2
+
+    def test_perimeter(self):
+        assert self.circle.perimeter() == 2 * math.pi * self.circle.radius
