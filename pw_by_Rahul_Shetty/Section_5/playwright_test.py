35f174e403cef43e35d9f28acd26c2bcaa78f7ea
SurajKukkipady <56889466+SurajKukkipady@users.noreply.github.com>
2025-08-05 16:30:53 +0530

    Update playwright_test.py

--- pw_by_Rahul_Shetty/Section_5/playwright_test.py
+++ pw_by_Rahul_Shetty/Section_5/playwright_test.py
@@ -2,4 +2,8 @@ def test_playwright_basics(playwright):
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
-    page.goto("https://rahulshettyacademy.com/seleniumPractise/#/")
+    page.goto("https://google.com")
+
+# chromium in headless by default
+def test_playwright_shortcut(page):
+    page.goto("https://google.com")
