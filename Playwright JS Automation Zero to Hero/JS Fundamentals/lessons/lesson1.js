7abb5f7fbc8ad885c8fd1159a1da1459e88d841b
SurajKukkipady <56889466+SurajKukkipady@users.noreply.github.com>
2025-08-09 17:24:49 +0530

    const

--- Playwright JS Automation Zero to Hero/JS Fundamentals/lessons/lesson1.js
+++ Playwright JS Automation Zero to Hero/JS Fundamentals/lessons/lesson1.js
@@ -1,2 +1,20 @@
 // 1. Hello World
 console.log("Hello, World!");
+
+// 2. Variables
+var firstName = "John";
+let lastName = "Smith";
+
+console.log(firstName, lastName);
+
+var age, dateOfBirth, sex
+age = 30;
+dateOfBirth = "1993-01-01"
+sex = "Male"
+
+console.log(age, sex)
+
+// 3. Constants
+const occupation = "Software Developer";
+//occupation = "Senior Software Developer"; // This will cause an error because constants cannot be reassigned
+console.log(occupation);
