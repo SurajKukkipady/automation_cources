a91f40b26acaefd45689c1eb473d9ab941a07220
SurajKukkipady <56889466+SurajKukkipady@users.noreply.github.com>
2025-08-11 17:20:07 +0530

    Create lesson5.js

---
+++ Playwright JS Automation Zero to Hero/JS Fundamentals/lessons/lesson5.js
@@ -0,0 +1,13 @@
+// Logical operators
+
+// && - logical AND
+console.log(true && true); // true
+console.log(true && false); // false
+
+// || - logical OR
+console.log(true || false); // true
+console.log(false || false); // false
+
+// ! - logical NOT
+console.log(!true); // false
+console.log(!false); // true
